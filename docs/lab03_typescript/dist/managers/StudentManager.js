9f46b627f65ab5a065fd7f5a1a9c7c4fa7315cbf
“Sittiphat” <sittiphat.t@kkumail.com>
2025-12-10 03:29:56 +0700

    2.5

--- docs/lab03_typescript/dist/managers/StudentManager.js
+++ docs/lab03_typescript/dist/managers/StudentManager.js
@@ -1,1 +1,36 @@
-"use strict";
+export class StudentManager {
+    constructor() {
+        this.students = [];
+    }
+    addStudent(student) {
+        this.students.push(student);
+        this.saveToLocalStorage();
+    }
+    getAllStudents() {
+        return this.students;
+    }
+    findStudentByID(id) {
+        return this.students.find(s => s.id === id);
+    }
+    findStudentsByName(name) {
+        const searchName = name.toLowerCase();
+        return this.students.filter(s => s.first_name.toLowerCase().includes(searchName) ||
+            s.last_name.toLowerCase().includes(searchName));
+    }
+    findStudentsByMajor(major) {
+        return this.students.filter(s => s.major.toLowerCase().includes(major.toLowerCase()));
+    }
+    findStudentByEmail(email) {
+        const searchEmail = email.toLowerCase();
+        return this.students.find(s => s.email.toLowerCase() === searchEmail);
+    }
+    saveToLocalStorage() {
+        localStorage.setItem("students", JSON.stringify(this.students));
+    }
+    loadFromLocalStorage() {
+        const data = localStorage.getItem("students");
+        if (data) {
+            this.students = JSON.parse(data);
+        }
+    }
+}
