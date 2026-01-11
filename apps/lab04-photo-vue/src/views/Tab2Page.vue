bef5778e8335787ee36ad4db14c11a578b1a52e4
“Sittiphat” <sittiphat.t@kkumail.com>
2026-01-12 00:30:25 +0700

    3.0

---
+++ apps/lab04-photo-vue/src/views/Tab2Page.vue
@@ -0,0 +1,43 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Photo Gallery</ion-title>
+      </ion-toolbar>  
+      <ion-toolbar>  
+        <div style="padding: 0 16px 10px 16px;">
+          <p style="margin: 0; font-size: 0.9rem; color: gray;">โดย นายสิทธิพรรธน์ ตันพิบูลย์   รหัส 663380027-3</p>
+        </div>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Photo Gallery</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" :key="photo.filepath" v-for="photo in photos">
+            <ion-img :src="photo.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <ion-fab-button @click="addNewToGallery()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { camera, trash, close } from 'ionicons/icons';
+import { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonImg } from '@ionic/vue';
+import { usePhotoGallery } from '@/composables/usePhotoGallery';
+const { photos, addNewToGallery } = usePhotoGallery();
+</script>
